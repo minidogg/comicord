f20c7fc50d8b5240cacf4a7079c44e81c706bc52
Crafterboy27 <117037081+minidogg@users.noreply.github.com>
2024-06-22 15:36:20 -0500

    start all over time

--- src/utils/fsUtils.js
+++
@@ -1,7 +0,0 @@
-import * as fs from 'fs';
-import * as path from 'path';
-
-export function makeFolder(folderPath, reset=false){
-    if(fs.existsSync(folderPath)&&reset==true)fs.rmSync(folderPath, {recursive: true});
-    if(!fs.existsSync(folderPath))fs.mkdirSync(folderPath);
-}
